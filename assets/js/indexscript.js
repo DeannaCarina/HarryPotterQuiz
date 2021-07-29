b3f8630802af0e20b89f6508866cc3b81f78cd27
DeannaCarina <deannacarina@hotmail.com>
2021-07-29 20:58:26 +0000

    add: Pop up to index for game instructions

---
+++ assets/js/indexscript.js
@@ -0,0 +1,7 @@
+/* The following code is from w3c schools popup tutorial */
+
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
