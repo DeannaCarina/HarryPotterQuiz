06ba10b381569dd5f0343a4e4c7308d12948534d
DeannaCarina <deannacarina@hotmail.com>
2021-07-30 16:03:12 +0000

    add: New Modal to index for game instructions with stylings

--- assets/js/indexscript.js
+++ assets/js/indexscript.js
@@ -1,7 +1,22 @@
-/* The following code is from w3c schools popup tutorial */
+/* Following code from https://www.w3schools.com/howto/howto_css_modals.asp edited for use on the Harry Potter Quiz website*/
 
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on cross button, close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
